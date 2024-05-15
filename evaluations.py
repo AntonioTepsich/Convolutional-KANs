e7c421fcc8a5c77a5e8cb0cd3c79b11a0dee3b6a
Alex Bodner <alexbodner2004@gmail.com>
2024-05-15 19:45:29 -0300

    added evaluations

---
+++ evaluations.py
@@ -0,0 +1,151 @@
+def train(model, device, train_loader, optimizer, epoch, criterion):
+    """
+    Train the model for one epoch
+
+    Args:
+        model: the neural network model
+        device: cuda or cpu
+        train_loader: DataLoader for training data
+        optimizer: the optimizer to use (e.g. SGD)
+        epoch: the current epoch
+        criterion: the loss function (e.g. CrossEntropy)
+
+    Returns:
+        avg_loss: the average loss over the training set
+    """
+
+    model.to(device)
+    model.train()
+    train_loss = 0
+    # Process the images in batches
+    for batch_idx, (data, target) in enumerate(tqdm(train_loader)):
+        # Recall that GPU is optimized for the operations we are dealing with
+        data, target = data.to(device), target.to(device)
+
+        # Reset the optimizer
+        optimizer.zero_grad()
+
+        # Push the data forward through the model layers
+        output = model(data)
+
+        # Get the loss
+        loss = criterion(output, target)
+
+        # Keep a running total
+        train_loss += loss.item()
+
+        # Backpropagate
+        loss.backward()
+        optimizer.step()
+
+    # return average loss for the epoch
+    avg_loss = train_loss / (batch_idx+1)
+    # print('Training set: Average loss: {:.6f}'.format(avg_loss))
+    return avg_loss
+
+def test(model, device, test_loader, criterion):
+    """
+    Test the model
+
+    Args:
+        model: the neural network model
+        device: cuda or cpu
+        test_loader: DataLoader for test data
+        criterion: the loss function (e.g. CrossEntropy)
+
+    Returns:
+        test_loss: the average loss over the test set
+        accuracy: the accuracy of the model on the test set
+        precision: the precision of the model on the test set
+        recall: the recall of the model on the test set
+        f1: the f1 score of the model on the test set
+    """
+
+    model.eval()
+    test_loss = 0
+    correct = 0
+    all_targets = []
+    all_predictions = []
+
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+
+            # Get the predicted classes for this batch
+            output = model(data)
+
+            # Calculate the loss for this batch
+            test_loss += criterion(output, target).item()
+
+            # Calculate the accuracy for this batch
+            _, predicted = torch.max(output.data, 1)
+            correct += (target == predicted).sum().item()
+
+            # Collect all targets and predictions for metric calculations
+            all_targets.extend(target.view_as(predicted).cpu().numpy())
+            all_predictions.extend(predicted.cpu().numpy())
+
+    # Calculate overall metrics
+    precision = precision_score(all_targets, all_predictions, average='macro')
+    recall = recall_score(all_targets, all_predictions, average='macro')
+    f1 = f1_score(all_targets, all_predictions, average='macro')
+
+    # Normalize test loss
+    test_loss /= len(test_loader.dataset)
+    accuracy = correct / len(test_loader.dataset)
+
+    # print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%), Precision: {:.2f}, Recall: {:.2f}, F1 Score: {:.2f}\n'.format(
+    #     test_loss, correct, len(test_loader.dataset), accuracy, precision, recall, f1))
+
+    return test_loss, accuracy, precision, recall, f1
+def train_and_test_models(model, device, train_loader, test_loader, optimizer, criterion, epochs, scheduler):
+    """
+    Train and test the model
+
+    Args:
+        model: the neural network model
+        device: cuda or cpu
+        train_loader: DataLoader for training data
+        test_loader: DataLoader for test data
+        optimizer: the optimizer to use (e.g. SGD)
+        criterion: the loss function (e.g. CrossEntropy)
+        epochs: the number of epochs to train
+        scheduler: the learning rate scheduler
+
+    Returns:
+        all_train_loss: a list of the average training loss for each epoch
+        all_test_loss: a list of the average test loss for each epoch
+        all_test_accuracy: a list of the accuracy for each epoch
+        all_test_precision: a list of the precision for each epoch
+        all_test_recall: a list of the recall for each epoch
+        all_test_f1: a list of the f1 score for each epoch
+    """
+    # Track metrics
+    all_train_loss = []
+    all_test_loss = []
+    all_test_accuracy = []
+    all_test_precision = []
+    all_test_recall = []
+    all_test_f1 = []
+
+    for epoch in range(1, epochs + 1):
+        # Train the model
+        train_loss = train(model, device, train_loader, optimizer, epoch, criterion)
+        all_train_loss.append(train_loss)
+
+        # Test the model
+        test_loss, test_accuracy, test_precision, test_recall, test_f1 = test(model, device, test_loader, criterion)
+        all_test_loss.append(test_loss)
+        all_test_accuracy.append(test_accuracy)
+        all_test_precision.append(test_precision)
+        all_test_recall.append(test_recall)
+        all_test_f1.append(test_f1)
+
+        print(f'End of Epoch {epoch}: Train Loss: {train_loss:.6f}, Test Loss: {test_loss:.4f}, Accuracy: {test_accuracy:.2%}')
+        scheduler.step()
+    model.all_test_accuracy = all_test_accuracy
+    model.all_test_precision = all_test_precision
+    model.all_test_f1 = all_test_f1
+    model.all_test_recall = all_test_recall
+
+    return all_train_loss, all_test_loss, all_test_accuracy, all_test_precision, all_test_recall, all_test_f1
