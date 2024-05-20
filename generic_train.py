04e5911a5deec1f61919556cf2b239c174f50abe
Alex Bodner <alexbodner2004@gmail.com>
2024-05-20 14:48:27 -0300

    agregado  path

--- generic_train.py
+++ generic_train.py
@@ -3,7 +3,7 @@ def count_parameters(model):
 from evaluations import train_and_test_models
 import torch.nn as nn
 import torch.optim as optim
-def train_model_generic(model, train_loader, test_loader,device,epochs= 15):
+def train_model_generic(model, train_loader, test_loader,device,epochs= 15,path =  "drive/MyDrive/KANs/models"):
     model.to(device)
     print("Params start",count_parameters(model))
 
@@ -11,6 +11,6 @@ def train_model_generic(model, train_loader, test_loader,device,epochs= 15):
     scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.8)
     criterion = nn.CrossEntropyLoss()
 
-    all_train_loss, all_test_loss, all_test_accuracy, all_test_precision, all_test_recall, all_test_f1 = train_and_test_models(model, device, train_loader, test_loader, optimizer, criterion, epochs=epochs, scheduler=scheduler)
+    all_train_loss, all_test_loss, all_test_accuracy, all_test_precision, all_test_recall, all_test_f1 = train_and_test_models(model, device, train_loader, test_loader, optimizer, criterion, epochs=epochs, scheduler=scheduler,path= path)
     print("Params End",count_parameters(model))
     return all_train_loss, all_test_loss, all_test_accuracy, all_test_precision, all_test_recall, all_test_f1
