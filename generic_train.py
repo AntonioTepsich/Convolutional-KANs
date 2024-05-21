7f0f11fbad3af4939dcedf6ba1483fceb078d163
Alex Bodner <alexbodner2004@gmail.com>
2024-05-21 19:25:13 -0300

    vamos bien

--- generic_train.py
+++ generic_train.py
@@ -3,6 +3,7 @@ def count_parameters(model):
 from evaluations import train_and_test_models
 import torch.nn as nn
 import torch.optim as optim
+import time
 def train_model_generic(model, train_loader, test_loader,device,epochs= 15,path =  "drive/MyDrive/KANs/models"):
     model.to(device)
     print("Params start",count_parameters(model))
@@ -10,7 +11,10 @@ def train_model_generic(model, train_loader, test_loader,device,epochs= 15,path 
     optimizer = optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-4)
     scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.8)
     criterion = nn.CrossEntropyLoss()
-
+    start = time.perf_counter()
     all_train_loss, all_test_loss, all_test_accuracy, all_test_precision, all_test_recall, all_test_f1 = train_and_test_models(model, device, train_loader, test_loader, optimizer, criterion, epochs=epochs, scheduler=scheduler,path= path)
+    total_time = time.perf_counter() -start
     print("Params End",count_parameters(model))
-    return all_train_loss, all_test_loss, all_test_accuracy, all_test_precision, all_test_recall, all_test_f1
+    model.training_time = total_time/60
+    print("Total time (min)",total_time/60)
+    #return all_train_loss, all_test_loss, all_test_accuracy, all_test_precision, all_test_recall, all_test_f1
