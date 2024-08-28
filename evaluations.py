d81e462e0cebb97649f1419942db831aa7c61326
Alex Bodner <alexbodner2004@gmail.com>
2024-08-28 11:57:44 -0300

    added verbosity option

--- evaluations.py
+++ evaluations.py
@@ -103,7 +103,7 @@ def test(model, device, test_loader, criterion):
     #     test_loss, correct, len(test_loader.dataset), accuracy, precision, recall, f1))
 
     return test_loss, accuracy, precision, recall, f1
-def train_and_test_models(model, device, train_loader, test_loader, optimizer, criterion, epochs, scheduler, path = "drive/MyDrive/KANs/models"):
+def train_and_test_models(model, device, train_loader, test_loader, optimizer, criterion, epochs, scheduler, path = "drive/MyDrive/KANs/models",verbose = True):
     """
     Train and test the model
 
@@ -145,7 +145,8 @@ def train_and_test_models(model, device, train_loader, test_loader, optimizer, c
         all_test_precision.append(test_precision)
         all_test_recall.append(test_recall)
         all_test_f1.append(test_f1)
-        print(f'End of Epoch {epoch}: Train Loss: {train_loss:.6f}, Test Loss: {test_loss:.4f}, Accuracy: {test_accuracy:.2%}')
+        if verbose:
+            print(f'End of Epoch {epoch}: Train Loss: {train_loss:.6f}, Test Loss: {test_loss:.4f}, Accuracy: {test_accuracy:.2%}')
         if test_accuracy>best_acc and not path is None:
             best_acc = test_accuracy
             torch.save(model,os.path.join(path,model.name+".pt"))
@@ -155,7 +156,8 @@ def train_and_test_models(model, device, train_loader, test_loader, optimizer, c
     model.all_test_precision = all_test_precision
     model.all_test_f1 = all_test_f1
     model.all_test_recall = all_test_recall
-    print("Best test accuracy", best_acc)
+    if verbose:
+        print("Best test accuracy", best_acc)
     return all_train_loss, all_test_loss, all_test_accuracy, all_test_precision, all_test_recall, all_test_f1
 
 def count_parameters(model):
