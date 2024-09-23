aacd6ce490dd8c5dafcc40d118bf8f7483be8b80
AlexBodner <61150961+AlexBodner@users.noreply.github.com>
2024-09-23 12:25:26 -0300

    Update CKAN_BN.py

--- architectures_28x28/CKAN_BN.py
+++ architectures_28x28/CKAN_BN.py
@@ -13,6 +13,8 @@ from kan_convolutional.kan_conv import KANConv2DLayer
 class CKAN_BN(nn.Module):
     def __init__(self,device: str = 'cpu'):
         super().__init__()
+        self.name = "KAN Conv BN & 2 Layer MLP"
+
         # self.conv1 = KAN_Convolutional_Layer(
         #     n_convs = 5,
         #     kernel_size= (3,3),
