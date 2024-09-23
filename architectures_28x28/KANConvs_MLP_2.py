f249c3ae82f86a1e99e5ff80ab01b791c8bd6986
AlexBodner <61150961+AlexBodner@users.noreply.github.com>
2024-09-23 12:20:24 -0300

    Update KANConvs_MLP_2.py

--- architectures_28x28/KANConvs_MLP_2.py
+++ architectures_28x28/KANConvs_MLP_2.py
@@ -8,20 +8,42 @@ from kan_convolutional.KANConv import KAN_Convolutional_Layer
 class KANC_MLP_2(nn.Module):
     def __init__(self,grid_size= 5):
         super().__init__()
-        self.conv1 = KAN_Convolutional_Layer(
-            n_convs = 5,
-            kernel_size= (3,3),
-            grid_size = grid_size
+        self.conv1 = KANConv2DLayer(
+            input_dim=1,
+            output_dim=5,
+            kernel_size=(3,3),
+            spline_order=3,
+            groups=1,
+            padding=0,
+            stride=1,
+            dilation=1,
+            grid_size=5,
+            base_activation=nn.SiLU,
+            grid_range=[0,1],
+            dropout=0.0,
         )
 
-        self.conv2 = KAN_Convolutional_Layer(
-            n_convs = 5,
-            kernel_size = (3,3),
-            dinamic_grid=True,
-            grid_size= grid_size
+        
 
+        # self.conv2 = KAN_Convolutional_Layer(
+        #     n_convs = 5,
+        #     kernel_size = (3,3),
+        #     device = device
+        # )
+        self.conv2 = KANConv2DLayer(
+            input_dim=5,
+            output_dim=25,
+            kernel_size=(3,3),
+            spline_order=3,
+            groups=1,
+            padding=0,
+            stride=1,
+            dilation=1,
+            grid_size=5,
+            base_activation=nn.SiLU,
+            grid_range=[0,1],
+            dropout=0.0,
         )
-
         self.pool1 = nn.MaxPool2d(
             kernel_size=(2, 2)
         )
