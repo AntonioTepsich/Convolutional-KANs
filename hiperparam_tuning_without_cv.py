51a0aafe63358006c5e76c083c824915effaa093
Alex Bodner <alexbodner2004@gmail.com>
2024-10-17 18:02:04 -0300

    removed device

--- hiperparam_tuning_without_cv.py
+++ hiperparam_tuning_without_cv.py
@@ -56,7 +56,7 @@ model_KANC_MLP= KANC_MLP()
 results_path = os.path.join("results",dataset_name)
 if not os.path.exists(results_path):
     os.mkdir(results_path)
-search_hiperparams_and_get_final_model(KANC_MLP,True, mnist_train,  test_loader,max_epochs= 20,path = path,search_grid_combinations = 10 ,folds = 1,dataset_name=dataset_name)
+search_hiperparams_and_get_final_model(KANC_MLP,True, mnist_train,  test_loader,max_epochs= 1,path = path,search_grid_combinations = 10 ,folds = 1,dataset_name=dataset_name)
 
 #Bigger
 model_KANC_MLP_2= KANC_MLP_2()
