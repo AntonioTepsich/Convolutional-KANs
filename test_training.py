8a91c9e0083e447b0da09a8d42d34e75bb9be170
Alex Bodner <alexbodner2004@gmail.com>
2024-10-31 11:34:41 -0300

    calculating training times

--- test_training.py
+++ test_training.py
@@ -4,6 +4,7 @@ from architectures_28x28.SimpleModels import *
 from evaluations import *
 from hiperparam_tuning import *
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 
 from torchvision.datasets import FashionMNIST
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -13,5 +14,6 @@ transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5
 mnist_train = FashionMNIST(root='./data', train=True, download=True, transform=transform)
 
 mnist_test = FashionMNIST(root='./data', train=False, download=True, transform=transform)
+test_loader = DataLoader(mnist_test, batch_size=64, shuffle=False)
 
-get_best_model(KANC_MLP_Big,epochs=20,config = {'lr': 0.0005, 'weight_decay': 1e-05, 'batch_size': 128}, train_obj= mnist_train,test_loader= mnist_test,path ="models\FashionMNIST" ,is_kan= True,grid_size = 20)
+get_best_model(KANC_MLP_Big,epochs=20,config = {'lr': 0.0005, 'weight_decay': 1e-05, 'batch_size': 128}, train_obj= mnist_train,test_loader= test_loader,path ="models\FashionMNIST" ,is_kan= True,grid_size = 20)
