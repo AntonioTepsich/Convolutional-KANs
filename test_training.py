07f286f5760ea5e2d01e413ee8aa613c0a9e23d8
Alex Bodner <alexbodner2004@gmail.com>
2024-10-31 12:05:52 -0300

    special train

--- test_training.py
+++ test_training.py
@@ -16,4 +16,4 @@ mnist_train = FashionMNIST(root='./data', train=True, download=True, transform=t
 mnist_test = FashionMNIST(root='./data', train=False, download=True, transform=transform)
 test_loader = DataLoader(mnist_test, batch_size=64, shuffle=False)
 
-get_best_model(KANC_MLP_Big,epochs=20,config = {'lr': 0.0005, 'weight_decay': 1e-05, 'batch_size': 128}, train_obj= mnist_train,test_loader= test_loader,path ="models\FashionMNIST" ,is_kan= True,grid_size = 20)
+get_best_model(KANC_MLP_Big,epochs=20,config = {'lr': 0.0005, 'weight_decay': 1e-05, 'batch_size': 128}, train_obj= mnist_train,test_loader= test_loader,path ="FashionMNIST" ,is_kan= True,grid_size = 20)
